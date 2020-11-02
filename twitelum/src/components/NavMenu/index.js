43a5171ce02635f82416cb5877f1ebf83c4aa623
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2020-11-02 07:34:11 -0300

    Exercício - CSS Modules

--- twitelum/src/components/NavMenu/index.js
+++ twitelum/src/components/NavMenu/index.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import "./navMenu.css"
+import navMenuStyles from "./navMenu.module.css"
 
 class NavMenu extends Component {
 
   render() {
     return (
-      < nav className="navMenu" >
-        <ul className="navMenu__lista">
-          <li className="navMenu__item">
-            <a className="navMenu__link">
+      <nav className={navMenuStyles.navMenu} >
+        <ul className={navMenuStyles.navMenu__lista}>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link}>
               Bem vindo(a): <br />
               <strong>{this.props.usuario}</strong>
             </a>
           </li>
-          <li className="navMenu__item">
-            <a className="navMenu__link" href="">Página Inicial</a>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="">Página Inicial</a>
           </li>
-          <li className="navMenu__item">
-            <a className="navMenu__link">Hashtags</a>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link}>Hashtags</a>
           </li>
-          <li className="navMenu__item">
-            <a className="navMenu__link">Logout</a>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link}>Logout</a>
           </li>
         </ul>
       </nav >
