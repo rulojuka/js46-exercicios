01efb889d4d43cb734a67da536a19fb150d30b2b
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2020-11-06 15:32:45 -0300

    Exercício - Autenticando as rotas

--- twitelum/src/routes.js
+++ twitelum/src/routes.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 // Páginas
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
+class PrivateRoute extends React.Component {
+  estaAutenticado = () =>{
+    return localStorage.getItem("TOKEN")
+  }
+
+  render(){
+    const	{	component:	ComponenteQueVeioNoParametro,	...props	}	=	this.props
+    if(this.estaAutenticado()){
+      return <ComponenteQueVeioNoParametro {...props} />
+    }else{
+      return <Redirect to="/login" />
+    }
+  }
+}
+
 class Roteamento extends React.Component {
   render() {
    return (
       <Switch>
-        <Route path="/" component={HomePage} exact />
+        <PrivateRoute path="/" component={HomePage} exact />
         <Route path="/login" component={LoginPage} />
       </Switch>
    );
