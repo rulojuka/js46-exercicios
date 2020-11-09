e5136d4d00aef0c7bbd004d5fe828dc686afeb4f
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2020-11-09 16:40:44 -0300

    Exercício - Organizando códigos - lidando com formulários mais complexos

--- twitelum/src/pages/LoginPage/index.js
+++ twitelum/src/pages/LoginPage/index.js
@@ -6,15 +6,65 @@ import { NotificacaoContext } from '../../context/NotificacaoContext'
 
 import './loginPage.css'
 
+const InputFormField = ({id, label, type, errors, values, onChange }) => {
+    return (
+        <div className="loginPage__inputWrap">
+            <label className="loginPage__label" htmlFor={id}>
+                {label}
+            </label>
+            <input
+                className="loginPage__input"
+                type={type}
+                id={id}
+                name={id}
+                value={values[id]}
+                onChange={onChange}
+            />
+
+            <p style={{color: "red"}}>{errors[id]}</p>
+        </div>
+    );
+};
+
 class LoginPage extends Component {
     static contextType = NotificacaoContext;
+
+    constructor(){
+        super()
+        this.state = {
+            values: {
+                inputLogin: "",
+                inputSenha: ""
+            },
+            errors: {}
+        }
+    }
+
+    onFormFieldChange = ({target}) => {
+        const value = target.value;
+        const name = target.name;
+        const values = { ...this.state.values, [name]:value }
+        this.setState({values}, () => {
+            this.formValidations();
+        })
+    }
+
+    formValidations = () => {
+        const { inputLogin, inputSenha } = this.state.values
+        const errors = {}
+
+        if(!inputLogin) errors.inputLogin = "Esse campo é obrigatório"
+        if(!inputSenha) errors.inputSenha = "Esse campo é obrigatório"
+
+        this.setState({errors})
+    };
     
     fazLogin = (evento) => {
         evento.preventDefault()
 
         const dadosDeLogin = {
-            login: this.inputLogin.value,
-            senha: this.inputSenha.value
+            login: this.state.values.inputLogin,
+            senha: this.state.values.inputSenha
         }
 
         LoginService.fazerLogin(dadosDeLogin)
@@ -38,14 +88,23 @@ class LoginPage extends Component {
                             <h2 className="loginPage__title">Seja bem vindo!</h2>
                             <form className="loginPage__form" action="/"
                                 onSubmit={this.fazLogin}>
-                                <div className="loginPage__inputWrap">
-                                    <label className="loginPage__label" htmlFor="login">Login</label> 
-                                    <input ref={ (inputLogin) => this.inputLogin = inputLogin } className="loginPage__input" type="text" id="login" name="login"/>
-                                </div>
-                                <div className="loginPage__inputWrap">
-                                    <label className="loginPage__label" htmlFor="senha">Senha</label> 
-                                    <input ref={ (inputSenha) => this.inputSenha = inputSenha } className="loginPage__input" type="password" id="senha" name="senha"/>
-                                </div>
+                                <InputFormField
+                                    id="inputLogin"
+                                    label="Login: "
+                                    type="text"
+                                    onChange={this.onFormFieldChange}
+                                    values={this.state.values}
+                                    errors={this.state.errors}
+                                    />
+                                
+                                <InputFormField
+                                    id="inputSenha"
+                                    label="Senha: "
+                                    type="password"
+                                    onChange={this.onFormFieldChange}
+                                    values={this.state.values}
+                                    errors={this.state.errors}
+                                    />
                                 {/* <div className="loginPage__errorBox">
                                     Mensagem de erro!
                                 </div> */}
